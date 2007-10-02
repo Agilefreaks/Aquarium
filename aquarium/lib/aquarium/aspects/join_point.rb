6b7356154945321cad45a5aab2821be5605f8e50
Dean Wampler <dean@polyglotprogramming.com>
2007-10-02 22:08:36 +0000

    #14053: replaced calls to jp#type with jp#target_type.

--- aquarium/lib/aquarium/aspects/join_point.rb
+++ aquarium/lib/aquarium/aspects/join_point.rb
@@ -153,7 +153,7 @@ module Aquarium
       alias :=== :eql?
   
       def inspect
-        "JoinPoint: {type = #{target_type.inspect}, object = #{target_object.inspect}, method_name = #{method_name}, instance_method? #{instance_method?}, context = #{context.inspect}}"
+        "JoinPoint: {target_type = #{target_type.inspect}, target_object = #{target_object.inspect}, method_name = #{method_name}, instance_method? #{instance_method?}, context = #{context.inspect}}"
       end
   
       alias :to_s :inspect
