c0638969bbb5e2142abb8d910e1b478549b5fa51
Dean Wampler <dean@polyglotprogramming.com>
2007-10-06 15:51:30 +0000

    #13410: website nav bar fixed. Some minor doc prepping for 0.1.6 release.

--- aquarium/lib/aquarium/version.rb
+++ aquarium/lib/aquarium/version.rb
@@ -9,7 +9,7 @@ module Aquarium
     unless defined? MAJOR
       MAJOR  = 0
       MINOR  = 1
-      TINY   = 5
+      TINY   = 6
       RELEASE_CANDIDATE = nil
       
       # RANDOM_TOKEN: 0.598704893979657
--- doc/docspec.rb
+++ doc/docspec.rb
@@ -1,6 +1,6 @@
 # Helper for webgen doco that works on both windows and posix
 # cd ../aquarium && ruby bin/spec
 Dir.chdir(File.dirname(__FILE__) + '/../aquarium') do
-  puts `ruby bin/spec #{ARGV.join(" ")}`
+  puts `rake spec #{ARGV.join(" ")}`
 end
 nil
