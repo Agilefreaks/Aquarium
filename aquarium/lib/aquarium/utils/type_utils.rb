9f9e3a25513bb4953ef4fe628bd1d0f3af12ce83
Dean Wampler <dean@polyglotprogramming.com>
2008-01-16 04:29:51 +0000

    Robustness improvements.

--- aquarium/lib/aquarium/utils/type_utils.rb
+++ aquarium/lib/aquarium/utils/type_utils.rb
@@ -6,20 +6,23 @@ module Aquarium
       end
 
       def self.descendents clazz
-        do_descendents clazz, Class, ["Class", "Module", "Object", clazz]
+        visited_types = ["Class", "Module", "Object", clazz]
+        do_descendents clazz, Class, visited_types
       end
       
       protected
       
       def self.do_descendents clazz, class_with_consts, visited
         result = [clazz]
-        visited << class_with_consts.name
+        visited << class_with_consts
+          # p "#{clazz}, #{class_with_consts}: #{class_with_consts.constants.inspect .gsub(/\</,"&lt;").gsub(/\>/,"&gt;")}<br/>"
         class_with_consts.constants.each do |const| 
-          next if const == clazz.name or visited.include?(const)
+          next if const == clazz.name 
           clazz2 = class_with_consts.class_eval "#{const}"
-          next unless clazz2.respond_to?(:ancestors)
+          next if visited.include?(clazz2) or not clazz2.respond_to?(:ancestors)
+          visited << clazz2
           result += [clazz2] if clazz2.ancestors.include? clazz
-          result += do_descendents(clazz, clazz2, visited) unless visited.include?(clazz2.name)
+          result += do_descendents(clazz, clazz2, visited) 
         end
         result.flatten.uniq
       end
