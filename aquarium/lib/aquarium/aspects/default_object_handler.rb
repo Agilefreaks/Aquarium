d8ee36a079be43c703cf0d8acf40581b4aacb156
Dean Wampler <dean@polyglotprogramming.com>
2008-01-21 19:12:59 +0000

    Saving work.

--- aquarium/lib/aquarium/aspects/default_object_handler.rb
+++ aquarium/lib/aquarium/aspects/default_object_handler.rb
@@ -1,12 +1,13 @@
 require 'aquarium/utils/array_utils'
+require 'set'
 
 module Aquarium
   module Aspects
-    # Some classes and modules support a :default_object flag and use it if no type or
+    # Some classes and modules support a :default_objects flag and use it if no type or
     # object is specified. For "convenience", requires that classes and modules including
-    # this module have a hash @specification defined with keys :default_object, :types,
+    # this module have a hash @specification defined with keys :default_objects, :types,
     # and :objects.
-    module DefaultObjectHandler
+    module DefaultObjectsHandler
       include Aquarium::Utils::ArrayUtils
       
       def default_objects_given
@@ -22,15 +23,15 @@ module Aquarium
         not default_objects_given.empty?
       end
       
-      def use_default_object_if_defined
+      def use_default_objects_if_defined
         return unless default_objects_given?
         default_objects_given.each do |object|
           if (object.kind_of?(Class) || object.kind_of?(Module))
             @specification[:types] ||= []
-            @specification[:types] << default_objects_given
+            @specification[:types] << object
           else
             @specification[:objects] ||= []
-            @specification[:objects] << default_objects_given
+            @specification[:objects] << object
           end
         end
       end
