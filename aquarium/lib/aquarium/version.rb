def5458c90b8635240048bca374faccee5e19076
Dean Wampler <dean@deanwampler.com>
2011-03-13 19:36:19 -0500

    V0.5.0 and refined the CHANGES notes.

--- aquarium/lib/aquarium/version.rb
+++ aquarium/lib/aquarium/version.rb
@@ -8,8 +8,8 @@ module Aquarium
 
     unless defined? MAJOR
       MAJOR  = 0
-      MINOR  = 4
-      TINY   = 4
+      MINOR  = 5
+      TINY   = 0
       RELEASE_CANDIDATE = nil
       
       # RANDOM_TOKEN: 0.598704893979657
