8c2b547df4ae242384870982c3e8f97925faf608
Dean Wampler <dean@concurrentthought.com>
2019-04-23 13:29:15 -0500

    Fixed path issue

--- aquarium/jruby/spec/spec_helper.rb
+++ aquarium/jruby/spec/spec_helper.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec/aquarium/spec_helper')
 
 require 'aquarium'
-require 'java/example.jar'
+require 'jruby/java/example.jar'
 include Java
